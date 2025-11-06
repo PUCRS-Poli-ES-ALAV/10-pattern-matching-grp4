37909843a26ce097aa2b9234079828077655b0d2
gustavoburgie <gustavoburgie@gmail.com>
2025-11-06 18:27:08 -0300

    implementado até o 2

--- Main.java
+++ Main.java
@@ -2,7 +2,11 @@ public class Main {
     public static void main(String[] args) {
         String s1 = "abcdefghij";
         String s2 = "efg";
+        
         System.out.println(pmatch(s1, s2));
+
+        System.out.println(search_RK(s2, s1));
+
     }
 
     public static int pmatch(String s1, String s2) {
@@ -19,21 +23,30 @@ public class Main {
         return 0;
     }
 
-    public int search_RK(){
+    public static int search_RK(String pat, String txt){
+        int tamPat = pat.length();
+        int tamTxt = txt.length();
+        long patHash = hash(pat, tamPat);
+        
+        for(int i = 0; i <= tamTxt - tamPat; i++){
+            long txtHash = hash(txt.substring(i, i+tamPat), tamPat);
+            if(patHash == txtHash)
+                return i; //ocorrencia ou colisão
+        }
 
-        return 0;
+        return tamTxt; //nenhuma ocorrencia
     }
 
-    public static long hash(String s, int M){
-        long hs = 0;
-        int R = 26;
-        int Q = 199;
+    public static long hash(String s, int patt){
+        long hash = 0;
+        int ALFBT = 26;
+        int modHash = 199; //número primo grande
 
-        for(int i = 0; i< M; i++){
-            hs = (hs * R + (int)s.charAt(i)) % Q;
+        for(int i = 0; i < patt; i++){
+            hash = (hash * ALFBT + (int)s.charAt(i)) % modHash;
         }
         
-        return 0;
+        return hash;
     }
 
 }
